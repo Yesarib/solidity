d0ba3af13a0182535839d256bb17647d7138fba1
Yesarib <yesaribaris23@gmail.com>
2024-09-04 18:59:00 +0300

    first commit

---
+++ contracts/SampleContract.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+
+contract SampleContract {
+    
+    uint public balanceReceived;
+    
+    function deposit() public payable {
+        balanceReceived += msg.value;
+    }
+
+    function getContractBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function withDrawAll() public {
+        address payable to = payable(msg.sender);
+
+        to.transfer(getContractBalance());
+    }
+
+    function withDrawToAddress(address payable to) public {
+        to.transfer(getContractBalance());
+    }
+}
